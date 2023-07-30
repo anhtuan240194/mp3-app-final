be06cfae4703ec1c74f6c6a074dae4345497d0b9
Anh Tuan <anhtuanvu241@gmail.com>
2023-07-30 11:07:51 +0700

    Ngày 30/7

--- src/dataSong.ts
+++ src/dataSong.ts
@@ -2,44 +2,44 @@ const dataSong = [
     {
         id:1,
         name: "Giá như chưa từng quen",
-        url: "src/assets/songs/GiaNhuChuaTungQuen.mp3",
+        url: "/assets/songs/GiaNhuChuaTungQuen.mp3",
         brand:"HKT - TV",
-        image: "src/assets/songs/GiaNhuChuaTungQuen.jpg"
+        image: "/assets/songs/GiaNhuChuaTungQuen.jpg"
     },
     {
         id:2,
         name: "Lệ tình",
-        url: "src/assets/songs/Letinh.mp3",
+        url: "/assets/songs/Letinh.mp3",
         brand:"HKT Brand",
-        image: "src/assets/songs/Letinh.jpg"
+        image: "/assets/songs/Letinh.jpg"
     },
     {
         id:3,
         name: "Mặt trái của sự thật",
-        url: "src/assets/songs/MatTraiCuaSuThat.mp3",
+        url: "/assets/songs/MatTraiCuaSuThat.mp3",
         brand:"HKT",
-        image: "src/assets/songs/MatTraiCuaSuThat.jpg"
+        image: "/assets/songs/MatTraiCuaSuThat.jpg"
     },
     {
         id:4,
         name: "Thêm một lần đau",
-        url:"src/assets/songs/ThemMotLanDau.mp3",
+        url:"/assets/songs/ThemMotLanDau.mp3",
         brand:"HKT",
-        image: "src/assets/songs/ThemMotLanDau.jpg"
+        image: "/assets/songs/ThemMotLanDau.jpg"
     },
     {
         id:5,
         name: "Trú mưa",
-        url:"src/assets/songs/TruMua.mp3",
+        url:"/assets/songs/TruMua.mp3",
         brand:"HKT",
-        image: "src/assets/songs/TruMua.jpg" 
+        image: "/assets/songs/TruMua.jpg" 
     },
     {
         id:6,
         name: "Nàng Kiểu lỡ bước",
-        url:"src/assets/songs/NangKieuLoBuoc.mp3",
+        url:"/assets/songs/NangKieuLoBuoc.mp3",
         brand:"HKT",
-        image: "src/assets/songs/NangKieuLoBuoc.jpg"
+        image: "/assets/songs/NangKieuLoBuoc.jpg"
     }
 ];
 export default dataSong
